5f6a794b29b72f169962e77893f2f6eff229ecf5
gamerYazilimci67 <168473901+gamerYazilimci67@users.noreply.github.com>
2024-10-13 14:03:03 +0300

    Update linuxsetup.sh

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -1,4 +1,8 @@
 #please run this file for best usage on linux 
 sudo chmod +x terminote.py
-mkdir /usr/bin/terminote
-sudo cp terminote.py /usr/bin/terminote
+sudo touch /usr/bin/terminote
+CURRENT_DIR=$(pwd)
+program_name="/terminote.py"
+FILE_PATH="$CURRENT_DIR$program_name"
+COMMAND="python $FILE_PATH"
+echo $COMMAND | sudo tee /usr/bin/terminote > /dev/null
